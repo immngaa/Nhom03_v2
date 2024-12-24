69b65d89ed14fce32b8ca12eb0e796428eb249a9
Thao <anh.ntt227080@sis.hust.edu.vn>
2024-12-24 22:20:20 +0700

    Tao form tiep nhan tu van

---
+++ Nhom03_v2/FormTiepNhanTuVan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nhom03_v2
+{
+	public partial class FormTiepNhanTuVan : Form
+	{
+		public FormTiepNhanTuVan()
+		{
+			InitializeComponent();
+		}
+
+		private void FormTiepNhanYeuCau_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
